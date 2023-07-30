e90c092cc1b91c0a3b9560679256a0f510f3df02
Ishmael MIRZAEE <IshmaelMIRZAEE@gmail.com>
2023-07-30 09:50:03 -0700

    Implement TypeScript version

---
+++ typescript/README.md
@@ -0,0 +1,5 @@
+# Age-based organizer
+
+> Asked in an interview, so it's not from the promised websites.
+
+
--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -1,26 +1,37 @@
-console.log("Try npm run lint/fix!");
-
-const longString = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ut aliquet diam.';
-
-const trailing = 'Semicolon'
-
-			const why={am:'I tabbed?'};
-
-const iWish = "I didn't have a trailing space..."; 
-
-const sicilian = true;;
-
-const vizzini = (!!sicilian) ? !!!sicilian : sicilian;
-
-const re = /foo   bar/;
-
-export function doSomeStuff(withThis: string, andThat: string, andThose: string[]) {
-    //function on one line
-    if(!Boolean(andThose.length)) {return false;}
-    console.log(withThis);
-    console.log(andThat);
-    console.dir(andThose);
-    console.log(longString, trailing, why, iWish, vizzini, re);
-    return;
+type Info = {
+  name: string;
+  age: number;
 }
-// TODO: more examples
+
+const input: Info[] = [
+  {name: 'Andrew D. Huberman', age: 48},
+  {name: 'Marco Rubio', age: 52},
+  {name: 'Lindsey Graham', age: 68},
+  {name: 'Ted Cruz', age: 52},
+  {name: 'Brian Tracy', age: 79},
+  {name: 'Jim Rohn', age: 79},
+  {name: 'Donald Trump', age: 77},
+  {name: 'Russell Westbrook', age: 34},
+  {name: "Luka Dončić", age: 25},
+  {name: 'LeBron James', age: 38},
+  {name: 'Chris Paul', age: 38},
+  {name: 'Kevin Durant', age: 34},
+  {name: 'Jayson Tatum', age: 25},
+  {name: 'P. J. Tucker', age: 38},
+  {name: "De'Anthony Melton", age: 25},
+  {name: "Jalen McDaniels", age: 25},
+]
+
+function main() {
+  const results:Record<number, string[]> = {};
+  input.map((item: Info) => {
+    if (!results[item.age]) {
+      results[item.age] = [];
+    }
+    results[item.age].push(item.name);
+  });
+  console.table(results)
+}
+
+
+main();
